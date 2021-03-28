b1cde4ce551189c4e45aea6aa9e49d88911a3abc
Jordan Beck <jordanfbeck@gmail.com>
2021-03-28 15:15:11 -0600

    app workign on local host, needs change to form_with syntax

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,5 +1,12 @@
 class PokemonController < ApplicationController
   def index
+# we change it from ".get" to ".new" to make a general call (you want this to be the base domain, so not https://pokeapi.co/api):
+# require "pry"; binding.pry
     pokemon = params[:pokemon]
+    conn = Faraday.new("https://pokeapi.co")
+# now we specify which "extension" we want to connect with:
+    response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
+# we create an instance variable since we will use it in our view, this is the information we will have access to by using the keys to retrieve values.
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
   end
 end
